afbd0247c44cdc77e131fd74286159e6578d5556
sunita6 <68330255+sunita6@users.noreply.github.com>
2020-07-22 10:51:22 +0530

    Add files via upload

---
+++ pythonassign1.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Hello python")
+
+
+# In[5]:
+
+
+print("hellopython")
+
+
+# In[6]:
+
+
+print("python's world")
+
+
+# In[7]:
+
+
+print("hello\npython")
+
+
+# In[8]:
+
+
+print("hello\apython")
+
+
+# In[10]:
+
+
+name ="abc"
+marks="90"
+age="25"
+print(f"the name of person is {name} ,marks is{marks},age is{age}")
+
+
+# In[13]:
+
+
+x=10
+y=10
+x==y
+
+
+# In[14]:
+
+
+id(x)
+
+
+# In[15]:
+
+
+id(y)
+
+
+# In[16]:
+
+
+del x
+
+
+# In[17]:
+
+
+id(y)
+
+
+# In[18]:
+
+
+id(x)
+
+
+# In[19]:
+
+
+5**7
+
+
+# In[20]:
+
+
+20+10
+
+
+# In[21]:
+
+
+12/8
+
+
+# In[22]:
+
+
+20/4
+
+
+# In[23]:
+
+
+20/3
+
+
+# In[24]:
+
+
+20//3
+
+
+# In[28]:
+
+
+a=2
+b=2
+c=3
+a==c
+
+
+# In[29]:
+
+
+a==b
+
+
+# In[30]:
+
+
+a!=b
+
+
+# In[31]:
+
+
+a|b
+
+
+# In[32]:
+
+
+bin(a)
+
+
+# In[33]:
+
+
+bin(b)
+
+
+# In[34]:
+
+
+bin(c)
+
+
+# In[37]:
+
+
+a>20 or 10 >9
+
+
+# In[38]:
+
+
+a>20 and 20>9
+
+
+# In[39]:
+
+
+str="hello python"
+"h" in str
+
+
+# In[40]:
+
+
+str="hello python"
+"h"  not in str
+
+
+# In[41]:
+
+
+20+20/29*4
+
+
+# In[43]:
+
+
+10+10/29*4
+
+
+# In[44]:
+
+
+2**-1
+
+
+# In[45]:
+
+
+a1=-6
+b1=-6
+
+
+# In[46]:
+
+
+id(a1)
+
+
+# In[47]:
+
+
+id(b1)
+
+
+# In[48]:
+
+
+10
+
+
+# In[49]:
+
+
+10>8>9
+
+
+# In[50]:
+
+
+10>8>4
+
+
+# In[ ]:
+
+
+
+
